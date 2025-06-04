73a99bbd7d67b536f29a75d24bd7b5f196e53340
oTyR3D <filipefonseca420@gmail.com>
2025-06-04 12:49:45 -0300

    feat: Criação da base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+export default function App() {
+  return (
+    <div>
+      <img src="react.svg" width={100} alt="" />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuaruio web e nativas</p>
+      <button type="button">Aprenda React</button>
+      <button type="button">Referencia da API</button>
+      <hr />
+      <h2>Crie Interfaces de usuario de componentes</h2>
+      <p>React permite que você construa interfaces de usuario a partir de pedaços individuais chamados componentes</p>
+      <hr />
+      <h2>Escreva componentes com código e marcação</h2>
+      <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada JSX. É uma extensão de sintaxe JavaScript popularizada pelo React</p>
+      <h2>Proximos passos</h2>
+      <ul>
+        <li>Uso de dados dinamicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos JavaScript</li>
+      </ul>
+    </div>
+  );
+}
